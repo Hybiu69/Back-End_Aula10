f684f49a43bad6151e10a97e3ba183f189bc0f31
LucasE. <90871939+Hybiu69@users.noreply.github.com>
2025-04-15 10:00:10 -0300

    Add files via upload

---
+++ DepFormativa/src/main/java/com/maps/DepFormativaApplication.java
@@ -0,0 +1,13 @@
+package com.maps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepFormativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepFormativaApplication.class, args);
+	}
+
+}
